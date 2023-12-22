55ec04f25e93208f81c3b89fa4ef1e819787969e
Dương Thanh Sang <148996728+sangduong1507@users.noreply.github.com>
2023-12-22 16:51:54 +0700

    Update button.js

--- js/button.js
+++ js/button.js
@@ -10,10 +10,11 @@
            }
          });
 
-//tìm kiếm
 document.querySelector('.search-box').addEventListener('submit', function(e) {
     e.preventDefault();  // Ngăn chặn hành động mặc định của form
-    const searchValue = document.getElementById('search-input').value.toLowerCase(); // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+
+     // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+    const searchValue = document.getElementById('search-input').value.toLowerCase();
     
     // Kiểm tra xem giá trị tìm kiếm có phải là "giải trí" hay không
     if (searchValue === 'giải trí') {
@@ -24,3 +25,79 @@ document.querySelector('.search-box').addEventListener('submit', function(e) {
         alert('Không tìm thấy kết quả cho từ khóa này!');
     }
 });
+
+document.querySelector('.search-box').addEventListener('submit', function(e) {
+    e.preventDefault();  // Ngăn chặn hành động mặc định của form
+
+     // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+    const searchValue = document.getElementById('search-input').value.toLowerCase();
+    
+    if (searchValue === 'trang chủ') {
+        // Chuyển hướng trang đến URL
+        window.location.href = 'https://break-fast-club.github.io/Bai-Cuoi-Ki/index.html';
+    } else {
+        // Xử lý logic khác
+        alert('Không tìm thấy kết quả cho từ khóa này!');
+    }
+});
+
+document.querySelector('.search-box').addEventListener('submit', function(e) {
+    e.preventDefault();  // Ngăn chặn hành động mặc định của form
+
+     // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+    const searchValue = document.getElementById('search-input').value.toLowerCase();
+    
+    if (searchValue === 'giới thiệu') {
+        // Chuyển hướng trang đến URL
+        window.location.href = 'https://break-fast-club.github.io/Bai-Cuoi-Ki/about.html';
+    } else {
+        // Xử lý logic khác
+        alert('Không tìm thấy kết quả cho từ khóa này!');
+    }
+});
+
+document.querySelector('.search-box').addEventListener('submit', function(e) {
+    e.preventDefault();  // Ngăn chặn hành động mặc định của form
+
+     // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+    const searchValue = document.getElementById('search-input').value.toLowerCase();
+    
+    if (searchValue === 'thực đơn') {
+        // Chuyển hướng trang đến URL
+        window.location.href = 'https://break-fast-club.github.io/Bai-Cuoi-Ki/menu.html';
+    } else {
+        // Xử lý logic khác
+        alert('Không tìm thấy kết quả cho từ khóa này!');
+    }
+});
+
+document.querySelector('.search-box').addEventListener('submit', function(e) {
+    e.preventDefault();  // Ngăn chặn hành động mặc định của form
+
+     // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+    const searchValue = document.getElementById('search-input').value.toLowerCase();
+    
+    if (searchValue === 'dịch vụ') {
+        // Chuyển hướng trang đến URL
+        window.location.href = 'https://break-fast-club.github.io/Bai-Cuoi-Ki/services.html';
+    } else {
+        // Xử lý logic khác
+        alert('Không tìm thấy kết quả cho từ khóa này!');
+    }
+});
+
+document.querySelector('.search-box').addEventListener('submit', function(e) {
+    e.preventDefault();  // Ngăn chặn hành động mặc định của form
+
+     // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+    const searchValue = document.getElementById('search-input').value.toLowerCase();
+    
+    if (searchValue === 'liên hệ') {
+        // Chuyển hướng trang đến URL
+        window.location.href = 'https://break-fast-club.github.io/Bai-Cuoi-Ki/contact.html';
+    } else {
+        // Xử lý logic khác
+        alert('Không tìm thấy kết quả cho từ khóa này!');
+    }
+});
+
