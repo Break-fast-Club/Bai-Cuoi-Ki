6141a7c6278930d70efe58a91e013e674e0617a6
Dương Thanh Sang <148996728+sangduong1507@users.noreply.github.com>
2023-12-22 16:32:01 +0700

    Update button.js

--- js/button.js
+++ js/button.js
@@ -9,3 +9,18 @@
              musicPlayer.pause();
            }
          });
+
+//tìm kiếm
+document.querySelector('.search-box').addEventListener('submit', function(e) {
+    e.preventDefault();  // Ngăn chặn hành động mặc định của form
+    const searchValue = document.getElementById('search-input').value.toLowerCase(); // Lấy giá trị nhập liệu và chuyển đổi thành chữ thường
+    
+    // Kiểm tra xem giá trị tìm kiếm có phải là "giải trí" hay không
+    if (searchValue === 'giải trí') {
+        // Chuyển hướng trang đến URL
+        window.location.href = 'https://break-fast-club.github.io/Bai-Cuoi-Ki/game.html';
+    } else {
+        // Xử lý logic khác
+        alert('Không tìm thấy kết quả cho từ khóa này!');
+    }
+});
